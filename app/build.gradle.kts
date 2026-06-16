865404d189c9d497913edd8ac3a62b38de7a7324
EmperorKabir <bhasin.kabir@gmail.com>
2026-06-16 02:29:28 +0100

    chore(release): bump to versionCode 34 / versionName 1.3.0 for publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.powermediaplayer"
         minSdk = 30
         targetSdk = 35
-        versionCode = 33
-        versionName = "1.2.0"
+        versionCode = 34
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
